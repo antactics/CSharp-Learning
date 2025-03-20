665b2e197e1ec20afbf35a34068f734538f0a8a6
antactics <142508000+antactics@users.noreply.github.com>
2025-03-20 12:25:23 +0900

    initial visual studio project

---
+++ 250320_GitHubLearning/250320_GitHubLearning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _250320_GitHubLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
